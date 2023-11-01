e2258d3c9e2512bd869cdf68a599cbd8b594eae5
mauro galindo <mauroeg73@gmail.com>
2023-11-01 04:06:58 -0300

    primera pre entrega JS

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+function futbol7()
+{
+    for(let i=2; i<=5; i++){
+    confirm ("que bien, estas para un fubol 5?")
+    alert("perfecto con vos somos " + i)
+    if(i==5) {alert ("estamos completos")
+    }
+
+}
+}
+
+let futbol = prompt("hola jugas al futbol?");
+if((futbol=="si")&&(futbol != "")){
+    futbol7() ;}
+else{ alert("ok, perdon las molestias")}    
+
+
+
+
+
+
+
+
+
+
+
