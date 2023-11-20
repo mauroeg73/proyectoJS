4c29d40056708a22f6e711333f3cd759d75d875e
mauro galindo <mauroeg73@gmail.com>
2023-11-20 15:46:04 -0300

    segunta pre entrega

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 
+// crear una lista para futbol5, preguntar quien puede, y si no puede incluir otro jugador
 
-function futbol7()
-{
-    for(let i=2; i<=5; i++){
-    confirm ("que bien, estas para un fubol 5?")
-    alert("perfecto con vos somos " + i)
-    if(i==5) {alert ("estamos completos")
+const equipo = ["martin","marco","agustin","tobias","facundo"]
+
+
+equipo.forEach(function(x) {
+    let confirmacion = confirm(x + " podes jugar?");
+    if(confirmacion == true){
+        alert("perfecto contamos con vos")
     }
-
-}
-}
-
-let futbol = prompt("hola jugas al futbol?");
-if((futbol=="si")&&(futbol != "")){
-    futbol7() ;}
-else{ alert("ok, perdon las molestias")}    
-
-
-
-
-
-
-
-
-
-
+    else{
+        let jugadorNuevo = prompt("entonces quien puede ir? ")
+        console.log (jugadorNuevo)
+    }
+    
+    })
