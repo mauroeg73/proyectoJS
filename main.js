0277c1ef3a5f15863730dbeb5ed77d9457ee71f1
mauro galindo <mauroeg73@gmail.com>
2023-12-06 20:09:56 -0300

    tercera preentrega

--- main.js
+++ main.js
@@ -1,48 +1,73 @@
 
-// crear una lista para futbol5, preguntar quien puede, y si no puede incluir otro jugador
+//  que el usuario cree la lista,
+//  almacenar la lista en local Storage,
+//  devolver la ultima lista almacenada,
+//  limpiar formulario
 
 
+  document.getElementById('agregarBtn').addEventListener('click', agregarJugador);
+  document.getElementById('traerBtn').addEventListener('click', traerUltimosResultados);
+  document.getElementById('limpiarBtn').addEventListener('click', limpiarFormulario);
 
-const Jugador = function(nombre,posicion){
-    this.nombre = nombre
-    this.posicion = posicion
+function agregarJugador() {
+  
+  let nombre = document.getElementById('nombre').value;
+  let posicion = document.getElementById('posicion').value;
+  let contacto = document.getElementById('contacto').value;
+
+  if (nombre && posicion && contacto) {
+    let lista = JSON.parse(localStorage.getItem('listaResultados')) || [];
+
+    if (lista.length < 5) {
+      lista.push(`Nombre: ${nombre}, Posición: ${posicion}, Contacto: ${contacto}`);
+      localStorage.setItem('listaResultados', JSON.stringify(lista));
+
+      mostrarResultadosActuales();
+    } else {
+      alert('se completo el equipo con exito');
+    }
+  } else {
+    alert('Por favor, completa todos los campos del formulario.');
+  }
 }
 
-let jugador1 = new Jugador("martin","arquero")
-let jugador2 = new Jugador("marco","defensa")
-let jugador3 = new Jugador("agustin","medio campo")
-let jugador4 = new Jugador("tobias","medio campo")
-let jugador5 = new Jugador("facundo","delantero")
+function traerUltimosResultados() {
+  let listaGuardada = localStorage.getItem('listaResultados');
 
+  if (listaGuardada) {
+    let lista = JSON.parse(listaGuardada);
+    let listaDOM = document.getElementById('resultado-lista');
+    listaDOM.innerHTML = '';
 
+    lista.forEach(function (elemento) {
+      let nuevoElemento = document.createElement('li');
+      nuevoElemento.textContent = elemento;
+      listaDOM.appendChild(nuevoElemento);
+    });
+  } else {
+    alert('No hay jugadores almacenados.');
+  }
+}
 
+  function limpiarFormulario() {
+  document.getElementById('miFormulario').reset();
 
-const equipo = [jugador1,jugador2,jugador3,jugador4,jugador5]
+  localStorage.removeItem('listaResultados');
+  document.getElementById('resultado-lista').innerHTML = '';
+}
 
+  function mostrarResultadosActuales() {
+  let listaGuardada = localStorage.getItem('listaResultados');
 
-equipo.forEach(function(x) {
-    let confirmacion = confirm(x.nombre + " podes jugar?");
-    if(confirmacion == true){
-        alert("perfecto contamos con vos")
-        console.table(x)
-    }
-    else{
-        crearJugador()
-    }
-    
-    })
+  if (listaGuardada) {
+    let lista = JSON.parse(listaGuardada);
+    let listaDOM = document.getElementById('resultado-lista');
+    listaDOM.innerHTML = '';
 
-    function crearJugador(){
-        let nombre = prompt("entonces quien puede ir?")
-        let posicion = prompt("y de que juega?")
-    
-        if(nombre == null || nombre === "" || false || posicion == null || posicion === ""){
-        alert("alguien tiene que ir, completa todos los datos por favor")
-        return(crearJugador())
-
-        }
-
-
-    let jugador = new Jugador(nombre,posicion)
-    console.table(jugador)
+    lista.forEach(function (elemento) {
+      let nuevoElemento = document.createElement('li');
+      nuevoElemento.textContent = elemento;
+      listaDOM.appendChild(nuevoElemento);
+    });
+  }
 }
