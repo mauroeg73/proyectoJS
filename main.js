16fdf8ec62d85b0c278684c0cc82249a7bfcded0
mauro galindo <mauroeg73@gmail.com>
2023-12-23 11:52:50 -0300

    ultima entrega js con api y librerias

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function agregarJugador() {
       alert('se completo el equipo con exito');
     }
   } else {
-    alert('Por favor, completa todos los campos del formulario.');
+    Swal.fire('Por favor, completa todos los campos del formulario.');
   }
 }
 
@@ -45,7 +45,7 @@ function traerUltimosResultados() {
       listaDOM.appendChild(nuevoElemento);
     });
   } else {
-    alert('No hay jugadores almacenados.');
+    Swal.fire("No hay jugadores almacenados.");
   }
 }
 
@@ -71,3 +71,62 @@ function traerUltimosResultados() {
     });
   }
 }
+
+
+
+window.addEventListener('load', ()=> {
+let lon
+let lat
+
+let tempvalor = document.getElementById('temp-valor')
+let ubicacion = document.getElementById('ubicacion')
+let icono = document.getElementById('icono')
+
+// capturo ubicacion del usuario por medio de la geolocalizacion
+
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition( posicion =>{
+    lon = posicion.coords.longitude
+    lat = posicion.coords.latitude
+
+// comienzo a consumir api de clima y utilizo librerias para mostar con iconos
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=33bf2110e869b369a7b65ff6c5d280e5&units=metric`
+
+    fetch(url)
+    .then(response => {return response.json()})
+    .then( data => {
+      let temp = Math.round (data.main.temp)
+      tempvalor.textContent = `${temp} °C`
+      console.log(data)
+
+      ubicacion.textContent = data.name
+
+      switch (data.weather[0].main){
+        case 'Rain':
+        icono.src = 'iconos/rainy-1.svg'
+        console.log('lluvia');
+        break;
+      case 'Clear':
+          icono.src = 'iconos/Day.svg'
+          console.log('limpio');
+          break;
+      case 'Clouds':
+          icono.src = 'iconos/cloudy.svg'
+          console.log('nubes');
+          break;
+      case 'Night':
+            icono.src = 'iconos/night.svg'
+            console.log('nubes');
+            break;    
+      }
+    })
+    .catch(error => {
+      console.log(error + "no se puede mostrar informacion")
+    })
+
+  })
+
+}
+
+})
